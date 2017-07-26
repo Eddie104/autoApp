d74e1c36dda68824224237817eacdefc008808da
hongjie <32968210@qq.com>
2017-07-26 15:44:52 +0800

    merge ios from jiaFang

--- ios/AutoApp/AppDelegate.m
+++ ios/AutoApp/AppDelegate.m
@@ -21,7 +21,9 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  //  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"AutoApp"
