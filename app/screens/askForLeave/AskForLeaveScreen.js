fefe6e69001de5a6b4471c98517a56393842a1fa
hongjie <32968210@qq.com>
2017-08-07 11:24:01 +0800

    添加了请假中请假类型的选择功能

--- app/screens/askForLeave/AskForLeaveScreen.js
+++ app/screens/askForLeave/AskForLeaveScreen.js
@@ -3,31 +3,138 @@
 import React, { PureComponent } from 'react';
 import {
 	StyleSheet,
-	View
+	View,
+	Text,
+	Image,
+	ScrollView,
+	TouchableOpacity
 } from 'react-native';
 
 import * as utils from '../../utils';
 import TopBar from '../../components/TopBar';
+import LeaveTypeModal from './LeaveTypeModal';
 
+/**
+ * 请假场景
+ */
 export default class AskForLeaveScreen extends PureComponent {
 	
 	constructor(props) {
 		super(props);
+		
+		this.state = {
+			// 是否显示请假类型的弹窗
+			isShowingLeaveTypeModal: false,
+			curType: '事假'
+		};
+
+		this._onLeaveTypePress = this.onLeaveTypePress.bind(this);
+		this._onLeaveDateStarPress = this.onLeaveDateStarPress.bind(this);
+		this._onLeaveDateEndPress = this.onLeaveDateEndPress.bind(this);
+
+		this._onLeaveTypeSelected = this.onLeaveTypeSelected.bind(this);
 	}
 
 	render() {
+		const { isShowingLeaveTypeModal, curType } = this.state;
 		return (
 			<View style={styles.container}>
 				<TopBar title={'请假'} showMoreBtn={false} />
-
-				
+				<ScrollView style={styles.container}>
+					<TouchableOpacity activeOpacity={0.8} onPress={this._onLeaveTypePress} style={styles.itemContainer}>
+						<Text style={styles.itemKey}>
+							请假类型
+						</Text>
+						<View style={styles.itemValContainer}>
+							<Text style={styles.itemKey}>
+								{ curType }
+							</Text>
+							<Image style={styles.arrow} source={require('../../imgs/arrow.png')} />
+						</View>
+					</TouchableOpacity>
+					<View style={styles.line} />
+					<TouchableOpacity activeOpacity={0.8} onPress={this._onLeaveDateStarPress} style={styles.itemContainer}>
+						<Text style={styles.itemKey}>
+							开始时间
+						</Text>
+						<View style={styles.itemValContainer}>
+							<Text style={styles.itemKey}>
+								事假
+							</Text>
+							<Image style={styles.arrow} source={require('../../imgs/arrow.png')} />
+						</View>
+					</TouchableOpacity>
+					<View style={styles.line} />
+					<TouchableOpacity activeOpacity={0.8} onPress={this._onLeaveDateEndPress} style={styles.itemContainer}>
+						<Text style={styles.itemKey}>
+							结束时间
+						</Text>
+						<View style={styles.itemValContainer}>
+							<Text style={styles.itemKey}>
+								事假
+							</Text>
+							<Image style={styles.arrow} source={require('../../imgs/arrow.png')} />
+						</View>
+					</TouchableOpacity>
+				</ScrollView>
+				<LeaveTypeModal visible={isShowingLeaveTypeModal} curType={curType} onSelected={this._onLeaveTypeSelected} />
 			</View>
 		);
+	}
+
+	onLeaveTypePress() {
+		this.setState({
+			isShowingLeaveTypeModal: true
+		});
+	}
+
+	onLeaveDateStarPress() {
+
+	}
+
+	onLeaveDateEndPress() {
+
+	}
+
+	onLeaveTypeSelected(curType) {
+		this.setState({
+			curType,
+			isShowingLeaveTypeModal: false
+		});
 	}
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1
+	},
+	itemContainer: {
+		flexDirection: 'row',
+		width: utils.screenWidth(),
+		height: utils.toDips(93),
+		backgroundColor: 'white',
+		alignItems: 'center',
+		paddingLeft: utils.toDips(20),
+		paddingRight: utils.toDips(20),
+		justifyContent: 'space-between'
+	},
+	itemKey: {
+		color: '#364153',
+		fontSize: utils.getFontSize(22),
+		backgroundColor: 'transparent'
+	},
+	itemValContainer: {
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	arrow: {
+		width: utils.toDips(18),
+		height: utils.toDips(32),
+		marginLeft: utils.toDips(38)
+	},
+	line: {
+		width: utils.screenWidth(),
+		height: utils.toDips(1.5),
+		backgroundColor: '#dddfe2'
 	}
 });
---
+++ app/screens/askForLeave/LeaveTypeModal.js
@@ -0,0 +1,144 @@
+'use strict';
+
+import React, { PureComponent, PropTypes } from 'react';
+import {
+	StyleSheet,
+	View,
+	Text,
+	Image,
+	Modal,
+	TouchableOpacity
+} from 'react-native';
+
+import * as utils from '../../utils';
+
+/**
+ * 请假类型选择弹窗
+ */
+export default class LeaveTypeModal extends PureComponent {
+
+	static propTypes = {
+		visible: PropTypes.bool,
+		typeArr: PropTypes.array,
+		curType: PropTypes.string,
+		onSelected: PropTypes.func
+	};
+
+	static defaultProps = {
+		visible: false,
+		typeArr: ['事假', '病假', '年假', '调休', '婚假', '产假', '陪产假', '路途假', '其他'],
+		curType: '事假',
+		onSelected: () => {}
+	};
+	
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			visible: props.visible
+		};
+
+		this._closeModal = this.closeModal.bind(this);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.visible === true) {
+			this.setState({
+				visible: true
+			});
+		}
+	}
+
+	render() {
+		const { visible } = this.state;
+		const { typeArr } = this.props;
+		return (
+			<Modal
+				// 可选值：slide、fade、none
+				animationType={'fade'}
+				transparent={true}
+				visible={visible}
+				onRequestClose={() => {console.warn("Modal has been closed.")}}
+			>
+				<View style={styles.modalContainer}>
+					<View style={styles.modalInnerContainer}>
+						{
+							typeArr.map((type, index) => {return this.renderItem(type, index);})
+						}
+					</View>
+				</View>
+			</Modal>
+		);
+	}
+
+	renderItem(type, index) {
+		const { typeArr, curType } = this.props;
+		return (
+			<TouchableOpacity
+				activeOpacity={0.8}
+				onPress={() => {
+					this._closeModal(type);
+				}}
+				key={index}
+			>
+				<View style={styles.itemContainer}>
+					<Text style={styles.itemKey}>{ type }</Text>
+					<Image style={styles.selectedImg} source={type === curType ? require('../../imgs/selected_quan.png') : require('../../imgs/unselected_quan.png')} />
+				</View>
+				{
+					index < typeArr.length - 1 && <View style={styles.line} />
+				}
+			</TouchableOpacity>
+		);
+	}
+
+	closeModal(val) {
+		this.setState({
+			visible: false
+		}, () => {
+			const { onSelected } = this.props;
+			onSelected && onSelected(val);
+		});
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	modalContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'rgba(0, 0, 0, 0.5)'
+	},
+	modalInnerContainer: {
+		width: utils.screenWidth() - utils.toDips(40 * 2),
+		alignItems: 'center',
+		backgroundColor: 'white'
+	},
+	itemContainer: {
+		flexDirection: 'row',
+		width: utils.screenWidth() - utils.toDips(40 * 2),
+		height: utils.toDips(93),
+		backgroundColor: 'white',
+		alignItems: 'center',
+		paddingLeft: utils.toDips(20),
+		paddingRight: utils.toDips(20),
+		justifyContent: 'space-between'
+	},
+	itemKey: {
+		color: '#364153',
+		fontSize: utils.getFontSize(22),
+		backgroundColor: 'transparent'
+	},
+	selectedImg: {
+		width: utils.toDips(36),
+		height: utils.toDips(36)
+	},
+	line: {
+		width: utils.screenWidth() - utils.toDips(40 * 2),
+		height: utils.toDips(1.5),
+		backgroundColor: '#dddfe2'
+	}
+});
